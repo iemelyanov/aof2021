e1c51f7b1e10a9f6c8f556ad2f239b792e8f9014
Igor Emelianov <cybercjp@gmail.com>
2021-12-07 00:06:16 +0200

    Day 6: merge part 1 and part 2

--- src/day6/p1/main.rs
+++ src/day6/p1/main.rs
@@ -1,16 +1,11 @@
 use std::io::{BufRead, BufReader};
 
-fn main() {
-    let input: Vec<String> = BufReader::new(std::io::stdin())
-        .lines()
-        .map(|l| l.unwrap())
-        .collect();
-    let input: Vec<usize> = input[0].split(",").map(|s| s.parse().unwrap()).collect();
-    let mut counters = vec![0; 9];
+fn f(input: &Vec<usize>, to_day: i32) -> i64 {
+    let mut counters = vec![0 as i64; 9];
     for i in input {
-        counters[i] += 1;
+        counters[*i] += 1;
     }
-    for _ in 1..=80 {
+    for _ in 1..=to_day {
         let t = counters[0];
         let mut j = 0;
         for i in 1..=8 {
@@ -23,5 +18,15 @@ fn main() {
             counters[8] = t;
         }
     }
-    println!("{}", counters.iter().sum::<i32>());
+    counters.iter().sum::<i64>()
+}
+
+fn main() {
+    let input: Vec<String> = BufReader::new(std::io::stdin())
+        .lines()
+        .map(|l| l.unwrap())
+        .collect();
+    let input: Vec<usize> = input[0].split(",").map(|s| s.parse().unwrap()).collect();
+    println!("After 80 day: {}", f(&input, 80));
+    println!("After 256 day: {}", f(&input, 256));
 }
