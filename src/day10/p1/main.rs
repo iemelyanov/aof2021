9483b514a59052947bdaa291a2853022dc8c49b3
Igor Emelianov <cybercjp@gmail.com>
2021-12-10 15:19:11 +0200

    Day 10 refactoring

--- src/day10/p1/main.rs
+++ src/day10/p1/main.rs
@@ -1,50 +1,49 @@
-use std::collections::LinkedList;
 use std::io::{BufRead, BufReader};
 
 fn main() {
     let lines = BufReader::new(std::io::stdin()).lines().map(|l| l.unwrap());
     let mut points = 0;
     for line in lines {
-        let mut chunks = LinkedList::new();
+        let mut chunks = Vec::with_capacity(line.len());
         for c in line.chars() {
             match c {
                 ')' => {
-                    if let Some(v) = chunks.back() {
+                    if let Some(v) = chunks.last() {
                         if *v != '(' {
                             points += 3;
                             break;
                         }
-                        chunks.pop_back();
+                        chunks.pop();
                     }
                 }
                 ']' => {
-                    if let Some(v) = chunks.back() {
+                    if let Some(v) = chunks.last() {
                         if *v != '[' {
                             points += 57;
                             break;
                         }
-                        chunks.pop_back();
+                        chunks.pop();
                     }
                 }
                 '>' => {
-                    if let Some(v) = chunks.back() {
+                    if let Some(v) = chunks.last() {
                         if *v != '<' {
                             points += 25137;
                             break;
                         }
-                        chunks.pop_back();
+                        chunks.pop();
                     }
                 }
                 '}' => {
-                    if let Some(v) = chunks.back() {
+                    if let Some(v) = chunks.last() {
                         if *v != '{' {
                             points += 1197;
                             break;
                         }
-                        chunks.pop_back();
+                        chunks.pop();
                     }
                 }
-                _ => chunks.push_back(c),
+                _ => chunks.push(c),
             }
         }
     }
--- src/day10/p2/main.rs
+++ src/day10/p2/main.rs
@@ -1,53 +1,36 @@
-use std::collections::LinkedList;
 use std::io::{BufRead, BufReader};
+
+fn match_pair(open: char, close: char) -> bool {
+    match open {
+        '(' => close == ')',
+        '[' => close == ']',
+        '{' => close == '}',
+        '<' => close == '>',
+        _ => unreachable!(),
+    }
+}
 
 fn main() {
     let lines = BufReader::new(std::io::stdin()).lines().map(|l| l.unwrap());
     let mut scores = Vec::new();
     for line in lines {
-        let mut chunks = LinkedList::new();
+        let mut chunks: Vec<char> = Vec::with_capacity(line.len());
         let mut corrupted = false;
         for c in line.chars() {
             match c {
-                ')' => {
-                    if let Some(v) = chunks.back() {
-                        if *v != '(' {
+                ')' | ']' | '>' | '}' => {
+                    if let Some(o) = chunks.last() {
+                        if !match_pair(*o, c) {
                             corrupted = true;
                             break;
                         }
-                        chunks.pop_back();
                     }
+                    chunks.pop();
                 }
-                ']' => {
-                    if let Some(v) = chunks.back() {
-                        if *v != '[' {
-                            corrupted = true;
-                            break;
-                        }
-                        chunks.pop_back();
-                    }
-                }
-                '>' => {
-                    if let Some(v) = chunks.back() {
-                        if *v != '<' {
-                            corrupted = true;
-                            break;
-                        }
-                        chunks.pop_back();
-                    }
-                }
-                '}' => {
-                    if let Some(v) = chunks.back() {
-                        if *v != '{' {
-                            corrupted = true;
-                            break;
-                        }
-                        chunks.pop_back();
-                    }
-                }
-                _ => chunks.push_back(c),
+                _ => chunks.push(c),
             }
         }
+
         if !corrupted {
             let mut score = 0 as i64;
             for c in chunks.iter().rev() {
@@ -57,7 +40,7 @@ fn main() {
                     '[' => 2,
                     '{' => 3,
                     '<' => 4,
-                    _ => 0,
+                    _ => unreachable!(),
                 };
             }
             scores.push(score);
